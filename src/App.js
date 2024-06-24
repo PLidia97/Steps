3165458f2cd60445623d854cefcfdcf4185fec62
Lidia <lidia_tanase97@yahoo.com>
2024-06-24 21:00:03 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import { useState } from "react";
 const messages = ["Learn React ", "Apply for jobs", "Invest your new income"];
 
 function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
-    <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
 
@@ -46,7 +55,7 @@ function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
 
